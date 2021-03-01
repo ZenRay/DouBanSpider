202f57332c9b3a4d0a371ae31d542aeae02d590e
ZenRay <jake2011ren@gmail.com>
2021-03-01 11:14:26 +0800

    feat($util $hammer): Check URL Validate
    检查 URL 石佛有效

--- DouBan/utils/hammers.py
+++ DouBan/utils/hammers.py
@@ -6,9 +6,10 @@ The script is a addictive tool that can deal with some special task:
 
 from __future__ import absolute_import
 
-import re
 from pypinyin import Style, pinyin
 
+import urllib3
+from requests import exceptions
 
 __all__ = ["extract1st_char"]
 
@@ -27,3 +28,36 @@ def extract1st_char(string):
     result = pinyin(string, style=Style.INITIALS, errors="default", strict=False)
     
     return "".join(i[0] for i in result).lower()
+
+
+
+def _url_valid(url):
+    """检查 URL 是否有效
+    
+    URL 有效性检验参考了 requests 模块 models 检验的代码
+    """
+    # Support for unicode domain names and paths.
+    try:
+        scheme, auth, host, port, path, query, fragment = urllib3.util.parse_url(url)
+    except exceptions.LocationParseError as e:
+        raise exceptions.InvalidURL(*e.args)
+
+    if not scheme:
+        error = ("Invalid URL {0!r}: No schema supplied. Perhaps you meant http://{0}?")
+        error = error.format(url)
+
+        raise exceptions.MissingSchema(error)
+
+    if not host:
+        raise exceptions.InvalidURL("Invalid URL %r: No host supplied" % url)
+
+    # In general, we want to try IDNA encoding the hostname if the string contains
+    # non-ASCII characters. This allows users to automatically get the correct IDNA
+    # behaviour. For strings containing only ASCII characters, we need to also verify
+    # it doesn't start with a wildcard (*), before allowing the unencoded hostname.
+    if not host:
+        raise exceptions.InvalidURL('URL has an invalid label.')
+    elif host.startswith('*'):
+        raise exceptions.InvalidURL('URL has an invalid label.')
+
+    return True
